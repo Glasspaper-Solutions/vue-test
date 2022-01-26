0f69f0a5521d244b824286b4fdfb37dad4e93f11
Glenn Nytveit <glenn@admin.glasspaper-solutions.no>
2022-01-26 10:34:33 +0100

    Added files

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <h1>Glasspaper Solution Store</h1>
+    <p>Velkommen til vår store</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
